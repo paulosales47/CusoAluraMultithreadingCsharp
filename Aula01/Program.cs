a8c092b8963f69f3865f4cbf34264f821ea6d0fc
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-06-23 14:25:36 -0300

    Utilizando o método Invoke da classe Parallel para realizar tarefas de forma simultanea

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -7,5 +7,42 @@ namespace Aula01
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+
+            //Serial(); //10020
+            Paralelo(); //5092
+
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+        }
+
+        public static void Serial()
+        {
+            Console.WriteLine("Executando tarefas de forma serial");
+            Tarefa1();
+            Tarefa2();
+        }
+
+        public static void Paralelo()
+        {
+            Console.WriteLine("Executando tarefas de forma paralela");
+            Parallel.Invoke(
+                () => Tarefa1(),
+                () => Tarefa2());
+        }
+
+        public static void Tarefa1()
+        {
+            Console.WriteLine("Executando tarefa 1...");
+            Thread.Sleep(5000);
+        }
+
+        public static void Tarefa2()
+        {
+            Console.WriteLine("Executando tarefa 2...");
+            Thread.Sleep(5000);
+        }
     }
 }
