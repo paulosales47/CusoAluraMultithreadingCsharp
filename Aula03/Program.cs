e43d7ec71d8f8706083ef8f4c40c3b388ee57387
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-07-07 09:20:42 -0300

    Criando array de tarefas e aguardando a finalização

--- Aula03/Program.cs
+++ Aula03/Program.cs
@@ -13,23 +13,38 @@ namespace Aula03
 
             Stopwatch watch = Stopwatch.StartNew();
 
-            Task tarefa1 = new Task(() => ExecutarTarefa(1));
-            tarefa1.Start();
-            tarefa1.Wait();
+            //Task tarefa1 = new Task(() => ExecutarTarefa(1));
+            //tarefa1.Start();
+            //tarefa1.Wait();
 
-            Task tarefa2 = Task.Run(() => ExecutarTarefa(2));
-            tarefa2.Wait();
+            //Task tarefa2 = Task.Run(() => ExecutarTarefa(2));
+            //tarefa2.Wait();
 
+            //Task<int> tarefa3 = Task.Run(() => ExecutarTarefaSoma(1, 3));
+            //Console.WriteLine($"Resultado da soma {tarefa3.Result}");
 
-            Task<int> tarefa3 = Task.Run(() => ExecutarTarefaSoma(1, 3));
-            Console.WriteLine($"Resultado da soma {tarefa3.Result}");
-
+            Corrida();
 
             watch.Stop();
-            Console.WriteLine(watch.ElapsedMilliseconds);
+            Console.WriteLine($"Tempo total: {watch.ElapsedMilliseconds}");
             
         }
 
+
+        public static void Corrida()
+        {
+            Console.WriteLine($"Numero de Threads inicio: {Process.GetCurrentProcess().Threads.Count}");
+            
+            Task[] tarefas = new Task[10];
+            for (int i = 0; i < 10; i++)
+            {
+                int idTarefa = i;
+                tarefas[i] = Task.Run(() => ExecutarTarefa(idTarefa));
+            }
+
+            Task.WaitAll(tarefas);
+            Console.WriteLine($"Numero de Threads fim: {Process.GetCurrentProcess().Threads.Count}");
+        }
 
         public static void ExecutarTarefa(int idTarefa)
         {
@@ -45,6 +60,7 @@ namespace Aula03
             Console.WriteLine($"Fim da tarefa de soma");
             return arg1 + arg2;
         }
+
 
 
 
